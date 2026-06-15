8245d8dc21c7947e1758f31471305e8432d7f680
Taiyo Koshiba <solluna.1119@gmail.com>
2026-06-15 12:21:59 +0900

    フォルダ構成移動

---
+++ MonsterBox/App/MonsterBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonsterBoxApp.swift
+//  MonsterBox
+//
+//  Created by Taiyo KOSHIBA on 2026/06/15.
+//
+
+import SwiftUI
+
+@main
+struct MonsterBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
